adab955a98c2aac6c0a30838994103accd5d3790
Przemek <p.ostrowinski@gmail.com>
2017-07-05 20:27:51 +0200

    adding randomize color for sec-hand

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function (event) {
     'use strict';
     const second = document.querySelector('.second-hand'),
         minutes = document.querySelector('.min-hand'),
         hours = document.querySelector('.hour-hand'),
         hand = document.querySelector('.hand'),
-        getHandTransitionTime = getComputedStyle(hand).transitionDuration,
-        getHandTransitionFun = getComputedStyle(hand).transitionTimingFunction;
+        getHandTransition = getComputedStyle(hand).transition;
 
     function clock() {
         const now = new Date();
@@ -21,25 +20,38 @@ document.addEventListener('DOMContentLoaded', function () {
         //Hours
         const getHours = now.getHours();
         const hourDegre = ((getHours / 12) * 360) + 90;
-
+        
+        //Randomize 6 chars hex color
+        function randomizeColor() {
+            const chars = '0123456789abcdef';
+            let color = '#';
+            
+            for (var i = 0; i < 6; i++) {
+                let setChar = chars[Math.floor(Math.random() * chars.length)];
+                color += setChar;
+            }
+            return color;
+        }
+        
         //Remove transition when any pointer it is on top (90 deg)
         function removeTrans(selectorDeg, number, selector) {
             if (selectorDeg === number) {
-                selector.style.transition = 'all 0s';
+                selector.style.transition = `all 0s`;
             } else {
-                selector.style.transition = `all ${getHandTransitionTime} ${getHandTransitionFun}`;
+                selector.style.transition = getHandTransition;
             }
         }
-
-        //Rotate second pointer
+        
+        //Rotate and change background color SECOND ponter
         second.style.transform = `rotate(${secondDegre}deg)`;
+        second.style.background = randomizeColor();
         removeTrans(secondDegre, 90, second);
 
-        //Rotate minutes pointer
+        //Rotate MINUTES pointer
         minutes.style.transform = `rotate(${minDegre}deg)`;
         removeTrans(minDegre, 90, minutes);
 
-        //Rotate hours pointer
+        //Rotate HOURS pointer
         hours.style.transform = `rotate(${hourDegre}deg)`;
         removeTrans(hourDegre, 12, hours);
     }
